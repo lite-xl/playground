591212e7fb37d77ea39423557e2b3ed44b34b2aa
takase1121 <20792268+takase1121@users.noreply.github.com>
2024-04-30 16:44:31 +0800

    core/plugins: make wasm_add_project_folder do a whole lot more

--- core/plugins/wasm_add_project_folder.lua
+++ core/plugins/wasm_add_project_folder.lua
@@ -6,23 +6,14 @@ local core = require "core"
 local connector = require "libraries.connector"
 
 if system.get_file_info(USERDIR .. "/.first") == nil then
-    local found = false
-    local home = os.getenv("HOME")
-    for _, v in ipairs(core.project_directories) do
-        if v.name == home then
-            found = true
-            break
-        end
-    end
-    -- work around old versions that allows duplicate entries
-    if not found then
-        core.add_project_directory(os.getenv('HOME'))
-    end
-    local f, err = io.open(USERDIR .. "/.first", "w")
-    if f == nil then
-        core.error("cannot open file: %s", err)
-        return
-    end
-    f:close()
-    connector.idbsync_save_sync()
+  core.add_project_directory("/usr/share/lite-xl")
+  core.root_view:open_doc(core.open_doc("/usr/share/lite-xl/welcome.md"))
+
+  local f, err = io.open(USERDIR .. "/.first", "w")
+  if f == nil then
+    core.error("cannot open file: %s", err)
+    return
+  end
+  f:close()
+  connector.idbsync_save_sync()
 end
