db038dfbcac0944e6f56fc6f0d7750da77b886e3
takase1121 <20792268+takase1121@users.noreply.github.com>
2024-06-05 08:16:36 +0800

    Revert "shell: experiment with esbuild and terser"
    This reverts commit ddab90ccf790c608654c56119bdc12679e899e00.

--- shell/esbuild.js
+++ shell/esbuild.js
@@ -1,27 +1,13 @@
 const installAndRun = require("./deps");
 
-installAndRun((outputFile, minify, entrypoint, ...inject) => {
+installAndRun((outputFile, minify, ...entrypoints) => {
   const esbuild = require("esbuild");
-  const { minify_sync } = require("terser");
-  const { writeFileSync } = require("fs");
-  const output = esbuild.buildSync({
-    entryPoints: [entrypoint],
+  esbuild.buildSync({
+    entryPoints: entrypoints,
     bundle: true,
-    minify: entrypoint.endsWith("js") ? false : minify == "true",
+    minify: minify === "true",
     target: "es2017",
     logLevel: "info",
     outfile: outputFile,
-    write: !entrypoint.endsWith("js"),
-    inject,
   });
-  if (entrypoint.endsWith("js") && minify == "true") {
-    const minified = minify_sync(output.outputFiles[0].text, {
-      compress: {
-        booleans_as_integers: true,
-        ecma: 2017,
-        passes: 3,
-      },
-    });
-    writeFileSync(outputFile, minified.code, { encoding: "utf8" });
-  }
 });
