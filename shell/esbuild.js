d966a210ceb8ca01d3912d7eed4f3a42bdb0e758
takase1121 <20792268+takase1121@users.noreply.github.com>
2024-11-08 19:38:21 +0800

    shell: remove minify option from esbuild script

--- shell/esbuild.js
+++ shell/esbuild.js
@@ -1,11 +1,11 @@
 const { argv } = require("process");
 const esbuild = require("esbuild");
 
-const [outputFile, bundle, minify, ...entrypoints] = argv.slice(2);
+const [outputFile, minify, ...entrypoints] = argv.slice(2);
 
 esbuild.buildSync({
   entryPoints: entrypoints,
-  bundle: bundle === "true",
+  bundle: true,
   minify: minify === "true",
   target: "es2017",
   logLevel: "info",
